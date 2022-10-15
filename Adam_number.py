e1a05ef3e94644f232ef3697f25361259f289cf6
sirishasiri210 <113820664+sirishasiri210@users.noreply.github.com>
2022-10-15 10:34:29 +0530

    n=int(input()) s=n**2 x=0 d=0 while n>0:     r=n%10     x=x*10+r     n=n//10 y=x**2 while y>0:     r=y%10     d=d*10+r     y=y//10 print(d==s)

--- Adam_number.py
+++ Adam_number.py
@@ -1,17 +1,14 @@
 n=int(input())
-s=n*n
-c=0
-k=0
-while n!=0:
-  r=n%10
-  c=c*10+r
-  n=n//10
-j=c**2
-while j!=0:
-    r1=j%10
-    k=k*10+r1
-    j=j//10
-if k==s:
-    print("True")
-else:
-    print("False")
+s=n**2
+x=0
+d=0
+while n>0:
+    r=n%10
+    x=x*10+r
+    n=n//10
+y=x**2
+while y>0:
+    r=y%10
+    d=d*10+r
+    y=y//10
+print(d==s)    
