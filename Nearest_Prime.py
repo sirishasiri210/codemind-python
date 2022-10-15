ab9f5bd367692cdf7c086261f80bb450d1048907
sirishasiri210 <113820664+sirishasiri210@users.noreply.github.com>
2022-10-15 10:58:23 +0530

    def is_prime(n):     fc=0     for i in range(1,(n+1)):         if n%i==0:             fc=fc+1     if fc==2:          return True     else:          return False
    def previous_prime(n):
        while is_prime(n)==False:
            n=n-1
        return n
        
    def next_prime(n):
        while is_prime(n)==False:
            n=n+1
        return n    
        
    def nearest_prime(n):
        a=previous_prime(n)
        b=next_prime(n)
        if n-a<=b-n:
            print(a)
        else:
            print(b)
            
    t=int(input())
    for i in range(t):
        x=int(input())
        nearest_prime(x)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,32 +1,33 @@
 def is_prime(n):
- fc=0
- for i in range(1,(n+1)):
-   if n%i==0:
-      fc=fc+1
- if fc==2:
-     return True
- else:
-     return False
-     
+    fc=0
+    for i in range(1,(n+1)):
+        if n%i==0:
+            fc=fc+1
+    if fc==2:
+         return True
+    else:
+         return False
+            
 def previous_prime(n):
-  while is_prime(n)==False:
-   n=n-1
-  return n
+    while is_prime(n)==False:
+        n=n-1
+    return n
+    
 def next_prime(n):
-  while is_prime(n)==False:
-    n=n+1
-  return n
-  
+    while is_prime(n)==False:
+        n=n+1
+    return n    
+    
 def nearest_prime(n):
-  a=previous_prime(n) 
-  b=next_prime(n)
-  if n-a<=b-n:
-     print(a)
-  else:
-    print(b)
+    a=previous_prime(n)
+    b=next_prime(n)
+    if n-a<=b-n:
+        print(a)
+    else:
+        print(b)
+        
 t=int(input())
 for i in range(t):
-  x=int(input())
-  nearest_prime(x)
-  
-     
+    x=int(input())
+    nearest_prime(x)
+            
